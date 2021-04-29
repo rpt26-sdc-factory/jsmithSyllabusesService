57f769bcebecd2e8777c00eda69e9190e5dbb549
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2021-04-28 22:24:12 -0500

    added functionality to database to delete by a supplied id

--- db/index.js
+++ db/index.js
@@ -27,5 +27,23 @@ const syllabus = (courseNumber, cb) => {
     });
 };
 
+const deleteEntry = (courseNumber, cb) => {
+  const options = {id: courseNumber};
+  SyllabusModel.deleteOne(options)
+    .then((success) => {
+      if (success.deletedCount === 0) {
+        cb('record not located and therefore not delete');
+      } else {
+        console.log('sucessfully deleted record inside of database', success);
+        cb(null, success);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      cb(err);
+    })
+};
+
 module.exports.hoursToComplete = hoursToComplete;
 module.exports.syllabus = syllabus;
+module.exports.deleteEntry = deleteEntry;
