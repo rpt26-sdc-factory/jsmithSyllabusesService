c9f16db88f6739781dab9063664c2f831b8b6de2
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2021-05-29 13:25:45 -0500

    CRUD complete!

--- db/postgres/index.js
+++ db/postgres/index.js
@@ -10,8 +10,14 @@ const pool = new Pool({
 
 // POST | C : create
 const insertSyllabus = (courseNumber, entry, cb) => {
-  pool.query()
-
+  pool.query(`INSERT INTO syllabi (id, syllabus) VALUES (${courseNumber}, '${JSON.stringify(entry)}')`, (err, result) => {
+    if (err) {
+      console.log('error inside of insertSyllabus');
+      cb(err, null);
+    } else {
+      cb(null, result);
+    }
+  });
 };
 
 // GET | R: read
@@ -19,9 +25,9 @@ const getHoursToComplete = (courseNumber, cb) => {
   pool.query(`SELECT syllabus FROM syllabi WHERE id = ${courseNumber}`, (err, result) => {
     if (result.rows[0] === undefined) {
       console.log('record doesn\'t exist');
-      cb('record doesn\'t exist', err);
+      cb('record doesn\'t exist', null);
     } else if (err) {
-      console.log('error while attempting to get hours to complete course from PostgreSQL database: ', err);
+      console.log('error inside of getHoursToComplete');
       cb(err, null);
     } else {
       let hoursToCompleteCourseData = result.rows[0].syllabus.hoursToCompleteCourse;
@@ -33,10 +39,9 @@ const getHoursToComplete = (courseNumber, cb) => {
 const getSyllabus = (courseNumber, cb) => {
   pool.query(`SELECT syllabus FROM syllabi WHERE id = ${courseNumber}`, (err, result) => {
     if (result.rows[0] === undefined) {
-      console.log('record doesn\'t exist');
-      cb('record doesn\'t exist', err);
+      cb('record doesn\'t exist', null);
     } else if (err) {
-      console.log('error while attempting to get syllabus from PostgreSQL database: ', err);
+      console.log('error inside of getSyllabus');
       cb(err, null);
     } else {
       let syllabusData = result.rows[0].syllabus;
@@ -47,14 +52,11 @@ const getSyllabus = (courseNumber, cb) => {
 
 // PUT | U: update
 const updateSyllabus = (courseNumber, edits, cb) => {
-  // IS THIS RIGHT ???????
-  // what if the edits are to more than one attribute of the json object
-  pool.query(`UPDATE syllabi SET syllabus = jsonb_set(syllabus, '{name}', '"my-other-name"') WHERE id = ${courseNumber}`, (err, result) => {
+  pool.query(`UPDATE syllabi SET syllabus = '${JSON.stringify(edits)}' WHERE id = ${courseNumber}`, (err, result) => {
     if (err) {
-      console.log('error while attempting to update syllabus within PostgreSQL database: ', err);
+      console.log('error inside of updateSyllabus');
       cb(err, null);
     } else {
-      console.log('result is: ', result);
       cb(null, result);
     }
   });
@@ -64,10 +66,9 @@ const updateSyllabus = (courseNumber, edits, cb) => {
 const deleteSyllabus = (courseNumber, cb) => {
   pool.query(`DELETE from syllabi WHERE id = ${courseNumber}`, (err, result) => {
     if (err) {
-      console.log('err while attempting to delete syllabus within PostgresSQL database: ', err);
+      console.log('error inside of deleteSyllabus');
       cb(err, null);
     } else {
-      console.log('result is: ', result);
       cb(null, result);
     }
   });
@@ -80,4 +81,11 @@ module.exports.getSyllabus = getSyllabus;
 module.exports.updateSyllabus = updateSyllabus;
 module.exports.deleteSyllabus = deleteSyllabus;
 
-let newData = {"id":800,"weeks":[{"lessons":[{"videos":[{"videoIndex":0,"videoTitle":"Weird weirdo test test see if this works","videoLengthMinutes":34,"videoLengthSeconds":24}],"readings":[{"readingIndex":0,"readingTitle":"Zimbabwe Metal virtual","readingLengthMinutes":60}],"exercises":[{"exerciseIndex":0,"exerciseTitle":"motivating quantify bluetooth feed","exerciseLengthMinutes":57}],"lessonIndex":0,"lessonTitle":"Chilean","videosLength":51,"readingsLength":13,"exercisesLength":10,"lessonDescription":"Ipsum porro sequi nemo at at cum quos accusantium vitae. Velit unde nisi et dignissimos. Illo aut voluptates. Ut et velit aliquam neque odit.","hoursToCompleteLesson":8}],"weekIndex":0,"hoursToCompleteWeek":25},{"lessons":[{"videos":[{"videoIndex":0,"videoTitle":"Exclusive connect monitor","videoLengthMinutes":58,"videoLengthSeconds":45}],"readings":[{"readingIndex":0,"readingTitle":"Steel","readingLengthMinutes":27}],"exercises":[{"exerciseIndex":0,"exerciseTitle":"local calculate solid state card","exerciseLengthMinutes":11}],"lessonIndex":0,"lessonTitle":"turquoise olive Generic","videosLength":52,"readingsLength":5,"exercisesLength":9,"lessonDescription":"Sit id facere voluptatum. Id doloribus neque error suscipit vitae veritatis et. Dolor molestias aut labore. Rerum natus porro asperiores et. Natus velit occaecati.","hoursToCompleteLesson":3}],"weekIndex":1,"hoursToCompleteWeek":93}],"hoursToCompleteCourse":33365};
+
+// either newData (where numbers are wrapped as strings)
+// or anotherSample (where numbers are numbers)
+// work for inserting / updating the database
+
+let newData = {"id":"800","weeks":[{"lessons":[{"videos":[{"videoIndex":"0","videoTitle":"Weird weirdo test test see if this works","videoLengthMinutes":"34","videoLengthSeconds":"24"}],"readings":[{"readingIndex":"0","readingTitle":"Zimbabwe Metal virtual","readingLengthMinutes":"60"}],"exercises":[{"exerciseIndex":"0","exerciseTitle":"motivating quantify bluetooth feed","exerciseLengthMinutes":"57"}],"lessonIndex":"0","lessonTitle":"Chilean","videosLength":"51","readingsLength":"13","exercisesLength":"10","lessonDescription":"Ipsum porro sequi nemo at at cum quos accusantium vitae. Velit unde nisi et dignissimos. Illo aut voluptates. Ut et velit aliquam neque odit.","hoursToCompleteLesson":"8"}],"weekIndex":"0","hoursToCompleteWeek":"25"},{"lessons":[{"videos":[{"videoIndex":"0","videoTitle":"Exclusive connect monitor","videoLengthMinutes":"58","videoLengthSeconds":"45"}],"readings":[{"readingIndex":"0","readingTitle":"Steel","readingLengthMinutes":"27"}],"exercises":[{"exerciseIndex":"0","exerciseTitle":"local calculate solid state card","exerciseLengthMinutes":"11"}],"lessonIndex":"0","lessonTitle":"turquoise olive Generic","videosLength":"52","readingsLength":"5","exercisesLength":"9","lessonDescription":"Sit id facere voluptatum. Id doloribus neque error suscipit vitae veritatis et. Dolor molestias aut labore. Rerum natus porro asperiores et. Natus velit occaecati.","hoursToCompleteLesson":"3"}],"weekIndex":"1","hoursToCompleteWeek":"93"}],"hoursToCompleteCourse":"33365"};
+
+let anotherSample = {"id":800,"weeks":[{"lessons":[{"videos":[{"videoIndex":0,"videoTitle":"Multi-lateral connect monitor","videoLengthMinutes":81,"videoLengthSeconds":12}],"readings":[{"readingIndex":0,"readingTitle":"Texas XSS transmitting deposit","readingLengthMinutes":66}],"exercises":[{"exerciseIndex":0,"exerciseTitle":"impactful quantify open-source interface","exerciseLengthMinutes":65}],"lessonIndex":0,"lessonTitle":"Pizza","videosLength":48,"readingsLength":44,"exercisesLength":3,"lessonDescription":"Eaque similique voluptatum voluptatibus et sint. Rerum voluptatem ut. Laudantium laborum aut rerum vel non laudantium eligendi dolor. Non qui aspernatur sunt.","hoursToCompleteLesson":8}],"weekIndex":0,"hoursToCompleteWeek":66}],"hoursToCompleteCourse":3333388888};
